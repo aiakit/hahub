8f85e8cf75022a67db8bd74b880ed44042435e85
mophite <1743299@qq.com>
2025-07-25 05:55:59 +0800

    bug fix

--- hub/intelligent/automation/light.go
+++ hub/intelligent/automation/light.go
@@ -22,20 +22,30 @@ func lightControl(c *ava.Context) {
 				var areaName = core.SpiltAreaName(e.AreaName)
 
 				name := strings.Split(e.OriginalName, " ")
+				buttonName := ""
+
+				for _, v1 := range name {
+					if v1 != " " && v1 != "" {
+						buttonName = v1
+						break
+					}
+				}
 
 				auto := &Automation{
-					Alias:       areaName + name[0] + "灯开关",
-					Description: "对" + areaName + "区域的" + name[0] + "进行控制",
+					Alias:       areaName + buttonName + "灯开关",
+					Description: "对" + areaName + "区域的" + buttonName + "进行控制",
 					Mode:        "single",
 				}
 				auto.Triggers = append(auto.Triggers, Triggers{
 					EntityID: e.EntityID,
 					Trigger:  "state",
 				})
+
 				for _, l := range v {
 					//找到名字相同的灯组
 					if l.Category == core.CategoryLightGroup {
-						if strings.Contains(l.Name, name[0]) {
+
+						if strings.Contains(l.Name, buttonName) {
 							auto.Actions = append(auto.Actions, ActionLight{
 								Action: "light.turn_on",
 								Data: &actionLightData{
